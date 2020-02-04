6c61fafb8fa0f99b9dae7eadcb1ad74ede453da1
Nostrum <daniel.soler@e-campus.uab.cat>
2020-02-04 13:21:09 +0100

    Reorder repo and make installable through source code

---
+++ tests/test_ppp.py
@@ -0,0 +1,9 @@
+import os
+import PPP.main as mn
+
+
+TEST_DIR = os.path.dirname(os.path.abspath(__file__))
+
+def test_ppp(pdb="1l63_proc.pdb", out="1l63_proc_processed.pdb"):
+    mn.main(os.path.join(TEST_DIR,pdb))
+    assert os.path.exists(os.path.join(TEST_DIR,out))
